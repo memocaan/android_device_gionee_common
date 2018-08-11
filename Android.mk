922015c6c7b06c5aef903be6cfcf4af2a4fc0723
memocaan <mehmetgumusgul@gmail.com>
2018-08-12 00:30:04 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter oppo oneplus,$(BOARD_VENDOR)),)
+ifneq ($(filter gionee oneplus,$(BOARD_VENDOR)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
